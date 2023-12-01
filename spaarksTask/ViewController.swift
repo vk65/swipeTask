b917f76c9193089b58cee2d31a5309124a9a452f
tirumala <vk7423267@gmail.com>
2023-12-01 17:56:36 +0530

    Initial Commit

---
+++ spaarksTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spaarksTask
+//
+//  Created by Tirumala on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
